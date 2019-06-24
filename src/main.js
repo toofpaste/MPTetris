21469cdad90e984e7aba006bc6feae323cc41107
Brian Hensley <brnhensley@gmail.com>
2019-06-24 10:16:42 -0700

    change rotate key to up

--- src/main.js
+++ src/main.js
@@ -218,9 +218,7 @@ document.addEventListener('keydown', event => {
         playerMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
-    } else if (event.keyCode === 81) {
-        playerRotate(-1);
-    } else if (event.keyCode === 87) {
+    } else if (event.keyCode === 38) {
         playerRotate(1);
     }
 });
